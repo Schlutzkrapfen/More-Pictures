b047db606c1c593a740823878a05e7144822a2af
Schlutzkrapfen <david.messner@protonmail.com>
2026-04-27 15:42:55 +0200

    updated the Ui -isStillnotActive

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "src
 from nicegui import ui
 from downloader import load_setup_conf,connect_label_studio,fetch_tasks,save_tasks,download_images,load_picture_conf,get_local_json,get_local_picutrs
 from converter import  adjust_brightness,add_guasianfilter
-from gui import set_up_gui
+from gui import set_up_connection
 def run_pipeline():
     conf = load_setup_conf()
     
@@ -39,8 +39,8 @@ def run_pipeline():
             add_guasianfilter(changed_list,json_path,strength)
 
 def main():
-    set_up_gui(on_start=run_pipeline)  
-    ui.run()
+   set_up_connection(on_start=run_pipeline)  
+   ui.run()
 if __name__ in {"__main__", "__mp_main__"}:
     run_pipeline()
     #main()
